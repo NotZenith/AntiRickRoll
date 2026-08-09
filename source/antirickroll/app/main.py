3581e45e96b2cb430164a6570f5a573de138aaca
NotZenith <NotZenith@users.noreply.github.com>
2026-08-09 12:38:27 +0545

    docs: expand main function docstring

--- source/antirickroll/app/main.py
+++ source/antirickroll/app/main.py
@@ -4,7 +4,9 @@ import sys
 from antirickroll.app.application import AntiRickRollApp, handle_exception
 
 def main():
-    """Application entry point."""
+    """Application entry point.
+    Sets up the global exception hook and starts the application.
+    """
     sys.excepthook = handle_exception
     app = AntiRickRollApp()
     app.run()
